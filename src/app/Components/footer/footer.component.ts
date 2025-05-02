12a9ed343e40617b85a7472744ae45649f5e0b9f
Ahmad Restom <ahmadrestom11@gmail.com>
2025-05-03 00:11:09 +0300

    Done with footer

--- src/app/Components/footer/footer.component.ts
+++ src/app/Components/footer/footer.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NewSettlerComponent } from '../new-settler/new-settler.component';
+import { ButtonComponent } from "../button/button.component";
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
