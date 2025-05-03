95ccf133ee9ff780d134fce140e977707bc6255d
Ahmad Restom <ahmadrestom11@gmail.com>
2025-05-03 22:09:28 +0300

    Done instructions

--- src/app/Components/Recipe-Components/instructions/instructions.component.ts
+++ src/app/Components/Recipe-Components/instructions/instructions.component.ts
@@ -1,9 +1,10 @@
 import { Component, Input } from '@angular/core';
 import { Instruction } from '../../../Models/Instruction';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-instructions',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './instructions.component.html',
   styleUrl: './instructions.component.scss'
 })
