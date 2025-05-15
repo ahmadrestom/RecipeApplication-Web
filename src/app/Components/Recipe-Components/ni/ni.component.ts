6a3e2a4694f3b83797c65b3f30a473a4c97b1753
Ahmad Restom <ahmadrestom11@gmail.com>
2025-05-16 02:21:01 +0300

    Added units for nutirition facts

--- src/app/Components/Recipe-Components/ni/ni.component.ts
+++ src/app/Components/Recipe-Components/ni/ni.component.ts
@@ -1,16 +1,33 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { NutritionInformation } from '../../../Models/NutritionInformation';
 import { CommonModule } from '@angular/common';
 import { CapitalizePipe } from '../../../Pipes/capitalize.pipe';
 
 @Component({
   selector: 'app-ni',
-  imports: [CommonModule,CapitalizePipe],
+  imports: [CommonModule, CapitalizePipe],
   templateUrl: './ni.component.html',
   styleUrl: './ni.component.scss'
 })
-export class NiComponent {
+export class NiComponent implements OnInit {
 
   @Input() ni!: NutritionInformation;
+
+  map = new Map<string, string>();
+
+  ngOnInit(): void {
+    this.map.set('calories', 'kcal');
+    this.map.set('total_fat', 'g');
+    this.map.set('cholesterol', 'mg');
+    this.map.set('carbohydrates', 'g');
+    this.map.set('protein', 'g');
+    this.map.set('sugar', 'g');
+    this.map.set('sodium', 'mg');
+    this.map.set('fiber', 'g');
+    this.map.set('zinc', 'mg');
+    this.map.set('magnesium', 'mg');
+    this.map.set('potassium', 'mg');
+  }
+
 
 }
