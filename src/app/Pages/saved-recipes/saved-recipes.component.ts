e554b0c313dc2a3df080e38bdba33f6f608d0ac7
Ahmad Restom <ahmadrestom11@gmail.com>
2025-05-20 00:47:47 +0300

    Empty saved recipes page done

--- src/app/Pages/saved-recipes/saved-recipes.component.ts
+++ src/app/Pages/saved-recipes/saved-recipes.component.ts
@@ -1,12 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, OnInit } from '@angular/core';
 import { AuthServiceService } from '../../Services/auth.service';
 import { Recipe } from '../../Models/recipe';
 import { RecipeContainerComponent } from "../../Components/recipe-container/recipe-container.component";
+import { HeaderComponent } from "../../Components/header/header.component";
+import { FooterComponent } from "../../Components/footer/footer.component";
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-saved-recipes',
-  imports: [RecipeContainerComponent],
+  imports: [RecipeContainerComponent, HeaderComponent, FooterComponent, CommonModule, RouterLink],
   templateUrl: './saved-recipes.component.html',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   styleUrl: './saved-recipes.component.scss'
 })
 export class SavedRecipesComponent implements OnInit{
