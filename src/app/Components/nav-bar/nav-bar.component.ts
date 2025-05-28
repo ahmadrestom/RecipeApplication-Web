939599190908a9dfd95125202cae399f52c8439f
Ahmad Restom <ahmadrestom11@gmail.com>
2025-05-28 20:19:44 +0300

    Styling for nav component

--- src/app/Components/nav-bar/nav-bar.component.ts
+++ src/app/Components/nav-bar/nav-bar.component.ts
@@ -1,14 +1,29 @@
 import { Component } from '@angular/core';
 import { ProfileModalComponent } from '../../Shared/profile-modal/profile-modal.component';
-import { NgIf } from '@angular/common';
-import { RouterLink } from '@angular/router';
+import { NgFor, NgIf } from '@angular/common';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [ProfileModalComponent, NgIf, RouterLink],
+  imports: [ProfileModalComponent, NgIf, RouterLink, NgFor],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss'
 })
 export class NavBarComponent {
   showProfile = false;
+
+    navItems = [
+      {label: 'Home', link:'/home'},
+      {label: 'Notifications', link: '/notification'},
+      {label: 'Saved Recipes', link: '/saved-recipes'},
+      {label: 'Profile', action: () => this.showProfile = true}
+    ];
+
+    constructor(private router: Router) {}
+
+  isActive(link: string | undefined): boolean {
+    return link ? this.router.isActive(link, true) : false;
+  }
+
+  
 }
